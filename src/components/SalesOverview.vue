38e8c15d5e8169f4baab63589bfe5c584efaa342
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-09-23 14:31:18 +0200

    Extracted some chart related logic to LineChart

--- src/components/SalesOverview.vue
+++ src/components/SalesOverview.vue
@@ -1,148 +1,12 @@
 <script setup>
 
 import { computed, onMounted, ref } from 'vue';
-import { Line } from 'vue-chartjs'
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip,
-    Legend,
-} from 'chart.js'
+
 import { formatNumber } from '@/utils/formatNumber';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip,
-    Legend
-)
-
-const alternatingBackgroundColors = ['#35387e', '#2e316d'];
-
-const backgroundPlugin = {
-    id: 'backgroundPlugin',
-    beforeDraw: (chart) => {
-        const { ctx, chartArea } = chart;
-        const { top, bottom } = chartArea;
-        const labels = chart.data.labels;
-
-        for (let i = 0; i < labels.length - 1; i++) {
-            ctx.fillStyle = alternatingBackgroundColors[i % 2];
-            const xStart = chart.scales.x.getPixelForTick(i);
-            const xEnd = chart.scales.x.getPixelForTick(i + 1);
-            ctx.fillRect(xStart, top, xEnd - xStart, bottom - top);
-        }
-    }
-};
-
-const thresholdPlugin = {
-    id: 'thresholdPlugin',
-    beforeDraw: (chart) => {
-        const { ctx, chartArea } = chart;
-        const { left, right } = chartArea;
-
-        ctx.save();
-        ctx.strokeStyle = '#424586';
-        ctx.setLineDash([5, 5]); // Dashed line style
-
-        // Upper threshold line at 20000
-        const upperY = chart.scales.y.getPixelForValue(20000);
-        ctx.beginPath();
-        ctx.moveTo(left, upperY);
-        ctx.lineTo(right, upperY);
-        ctx.stroke();
-
-        // Lower threshold line at 10000
-        const lowerY = chart.scales.y.getPixelForValue(10000);
-        ctx.beginPath();
-        ctx.moveTo(left, lowerY);
-        ctx.lineTo(right, lowerY);
-        ctx.stroke();
-
-        ctx.restore();
-    }
-};
-
-const hoverLinePlugin = {
-    id: 'hoverLinePlugin',
-    afterDatasetsDraw: (chart) => {
-        const { ctx, tooltip } = chart;
-
-        if (tooltip._active && tooltip._active.length) {
-            const activePoint = tooltip._active[0];
-            const x = activePoint.element.x;
-            ctx.setLineDash([5, 5]); // Dashed line style
-
-            ctx.save();
-            ctx.strokeStyle = '#7173a4';
-            ctx.lineWidth = 2;
-            ctx.beginPath();
-            ctx.moveTo(x, chart.chartArea.top);
-            ctx.lineTo(x, chart.chartArea.bottom);
-            ctx.stroke();
-            ctx.restore();
-        }
-    }
-};
+import LineChart from './LineChart.vue';
 
 const loaded = ref(false)
 const data = ref(null)
-
-const options = {
-    maintainAspectRatio: false,
-    responsive: true,
-    scales: {
-        x: {
-            grid: {
-                display: true,
-            },
-            ticks: {
-                color: '#9E9E9E',
-            },
-        },
-        y: {
-            beginAtZero: true,
-            max: 25000,
-            ticks: {
-                color: '#9E9E9E',
-                stepSize: 10000,
-                callback: (value) => {
-                    // Format the value as $Xk
-                    if (value === 0) {
-                        return '$0'
-                    }
-                    return '$' + (value / 1000).toFixed(0) + 'k';
-                }
-            },
-            grid: {
-                display: false,
-            },
-            position: 'right'
-        }
-    },
-    plugins: {
-        tooltip: {
-            mode: 'nearest',
-            intersect: false,
-            callbacks: {
-                label: function (context) {
-                    return ' Revenue: ' + context.raw.toLocaleString();
-                }
-            },
-            displayColors: true,
-        },
-        legend: {
-            display: false,
-        },
-    },
-}
-
-const plugins = [backgroundPlugin, thresholdPlugin, hoverLinePlugin]
 
 onMounted(async () => {
     try {
@@ -187,8 +51,6 @@ const revenueData = ref([
                 </div>
             </div>
         </header>
-        <div class="position-relative" :style="{ height: '250px' }">
-            <Line :data :options :plugins />
-        </div>
+        <LineChart :data label="Revenue" height="250px" />
     </v-card>
 </template>
